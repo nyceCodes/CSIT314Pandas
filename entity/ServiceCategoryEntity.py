1c8828874693c1e56b008a5781ad3e98da8626d3
nyceCodes <sumcad001@mymail.sim.edu.sg>
2025-05-21 13:17:02 +0800

    PandaClean

--- entity/ServiceCategoryEntity.py
+++ entity/ServiceCategoryEntity.py
@@ -7,12 +7,6 @@ class ServiceCategoryEntity:
         categories = cur.fetchall(); cur.close(); conn.close()
         return categories
 
-    def get_category_by_id(self, category_id):
-        conn = get_connection(); cur = conn.cursor(dictionary=True)
-        cur.execute("SELECT * FROM service_categories WHERE id = %s", (category_id,))
-        cat = cur.fetchone(); cur.close(); conn.close()
-        return cat
-
     def create_category(self, name, description):
         conn = get_connection(); cur = conn.cursor()
         cur.execute("INSERT INTO service_categories (serviceCategoryName, serviceCategoryDescription) VALUES (%s, %s)", (name, description))
